e49fbf0b3b520c2f345544bd151da6296e9aebe4
Timothy Lin <timothywlin@gmail.com>
2019-08-21 22:46:39 -0500

    add migration script for vocabulary

---
+++ NihongoDatabase/Migration/Migration-Vocabulary.sql
@@ -0,0 +1,26 @@
+﻿PRINT (N'Executing Migration-Vocabulary')
+GO
+
+BEGIN TRANSACTION [MigrationVocabulary]
+
+    CREATE TABLE [#tVocabulary]
+    (
+        [English] VARCHAR(512)  NOT NULL,
+        [Kana]    NVARCHAR(512) NOT NULL,
+        [Kanji]   NVARCHAR(512) NULL
+    )
+
+    INSERT INTO [#tVocabulary] ([English], [Kana], [Kanji])
+    VALUES ('Book', N'ほん', N'本');
+
+    MERGE [dbo].[Vocabulary] [destination]
+    USING [#tVocabulary] [source]
+    ON [source].[English] = [destination].[English]
+    WHEN NOT MATCHED BY TARGET
+        THEN
+        INSERT ([English], [Kana], [Kanji], [CreatedBy], [CreatedDate])
+        VALUES ([source].[English], [source].[Kana], [source].[Kanji], 'system', GETUTCDATE());
+
+    DROP TABLE [#tVocabulary]
+
+COMMIT TRANSACTION [MigrationVocabulary]
