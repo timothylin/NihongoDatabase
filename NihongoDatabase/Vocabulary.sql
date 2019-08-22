44945a24d4ff20aaf7f9e74b9f9e514c52d9c308
Timothy Lin <timothywlin@gmail.com>
2019-08-21 20:53:00 -0500

    add vocab table

---
+++ NihongoDatabase/Vocabulary.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Vocabulary]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+	[English] VARCHAR(512) NOT NULL,
+	[Kana] NVARCHAR(512) NOT NULL,
+	[Kanji] NVARCHAR(512) NULL,
+	[CreatedBy] NVARCHAR(512) NOT NULL,
+	[CreatedDate] DATETIME NOT NULL,
+	[ModifiedBy] NVARCHAR(512) NULL,
+	[ModifiedDate] DATETIME NULL,
+	[IsDeleted] BIT NOT NULL DEFAULT(0)
+)
