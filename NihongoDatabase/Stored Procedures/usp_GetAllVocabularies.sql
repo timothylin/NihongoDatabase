e24346540e3925d063af8424aaae4ebdc256fb49
Timothy Lin <timothywlin@gmail.com>
2019-09-08 19:05:44 -0500

    added sproc for returning all vocabs or by ID

---
+++ NihongoDatabase/Stored Procedures/usp_GetAllVocabularies.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE [dbo].[usp_GetAllVocabularies]
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT [Id],
+           [English],
+           [Kana],
+           [Kanji],
+           [CreatedBy],
+           [CreatedDate],
+           [ModifiedBy],
+           [ModifiedDate],
+           [IsDeleted]
+    FROM [Vocabulary]
+    WHERE [IsDeleted] = 0;
+END
---
+++ NihongoDatabase/Stored Procedures/usp_GetVocabularyById.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE [dbo].[usp_GetVocabularyById]
+    @Id INT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT [Id],
+           [English],
+           [Kana],
+           [Kanji],
+           [CreatedBy],
+           [CreatedDate],
+           [ModifiedBy],
+           [ModifiedDate],
+           [IsDeleted]
+    FROM [Vocabulary]
+    WHERE [Id] = @Id
+      AND [IsDeleted] = 0;
+END
